c34230a0cbef4f56e48bd03225e0a26bb4b23b0b
Clive Murray <clive.murray@digital.justice.gov.uk>
2013-07-17 15:30:08 +0100

    Binned tests

--- lib/moj_frontend_toolkit_gem/engine.rb
+++
@@ -1,7 +0,0 @@
-module StaticAssets
-  class Engine < ::Rails::Engine
-    initializer 'moj_frontend_toolkit_gem.load_moj_frontend_toolkit_gem' do |app|
-      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
-    end
-  end
-end
